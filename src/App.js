ecc65027495f96e97a56c44a57280d23c42c72a7
Shao0203 <nzsyl0203@gmail.com>
2024-12-29 20:33:26 +0800

    handle request error

--- src/App.js
+++ src/App.js
@@ -8,16 +8,7 @@ function App() {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
-
-  async function getNotes(params) {
-    setLoading(true);
-    let url = 'http://localhost:8080/notes';
-    if (params) url += `?${new URLSearchParams({ term: params })}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    setNotes(data);
-    setLoading(false);
-  }
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     getNotes();
@@ -28,11 +19,28 @@ function App() {
     getNotes(e.target.value);
   }
 
+  async function getNotes(params) {
+    setLoading(true);
+    let url = 'http://localhost:8080/notes';
+    if (params) url += `?${new URLSearchParams({ term: params })}`;
+    const res = await fetch(url);
+    // if (res.status >= 400) {
+    if (!res.ok) {
+      const error = await res.json();
+      setError(error);
+    } else {
+      const data = await res.json();
+      setNotes(data);
+    }
+    setLoading(false);
+  }
+
   async function handleAdd(note) {
     const res = await fetch('http://localhost:8080/notes', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: 'Bearer SOMEJWTTOKEN',
       },
       body: JSON.stringify(note),
     });
@@ -46,6 +54,7 @@ function App() {
         <h1>我的笔记本</h1>
         <SearchNote searchTerm={searchTerm} onChange={handleSearch} />
         {loading ? <div>Loading...</div> : <NoteList notes={notes} />}
+        {error && <div style={{ color: 'red' }}>{error.message}</div>}
         <AddNote onSubmit={handleAdd} />
       </div>
     </main>
