ccbb563759dda4a6beaccd1860727e2682f69d0b
Bright <nzsyl0203@gmail.com>
2024-12-29 16:49:44 +0800

    Add loading and search.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,35 @@ import SearchNote from './components/SearchNote';
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  async function getNotes() {
-    const res = await fetch('http://localhost:8080/notes');
+  async function getNotes(params) {
+    setLoading(true);
+    let url = 'http://localhost:8080/notes';
+    if (params) url += `?${new URLSearchParams({ term: params })}`;
+    console.log(url);
+    const res = await fetch(url);
     const data = await res.json();
     setNotes(data);
+    setLoading(false);
   }
 
   useEffect(() => {
     getNotes();
   }, []);
 
+  function handleSearch(e) {
+    setSearchTerm(e.target.value);
+    getNotes(e.target.value);
+  }
+
   return (
     <main className='container'>
       <div>
         <h1>我的笔记本</h1>
-        <SearchNote />
-        <NoteList notes={notes} />
+        <SearchNote searchTerm={searchTerm} onChange={handleSearch} />
+        {loading ? <div>Loading...</div> : <NoteList notes={notes} />}
         <AddNote />
       </div>
     </main>
