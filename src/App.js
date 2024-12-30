346ddca5936da4d5fdb9276cb33ff56daed78076
Shao0203 <nzsyl0203@gmail.com>
2024-12-30 19:25:33 +0800

    useRef to get HTML DOM Element.

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
-import AddNote from './components/AddNote';
-import NoteList from './components/NoteList';
-import SearchNote from './components/SearchNote';
-// import request from './utils/request';
+import VideoPlayer from './components/VideoPlayer';
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [error, setError] = useState(null);
-
-  function handleSearch(e) {
-    setSearchTerm(e.target.value);
-    getNotes(e.target.value, new AbortController());
-  }
-
-  async function getNotes(params, controller) {
-    setLoading(true);
-    let url = '/api/notes';
-    if (params) url += `?${new URLSearchParams({ term: params })}`;
-
-    try {
-      const res = await axios.get(url, {
-        signal: controller.signal,
-      });
-      setNotes(res.data);
-    } catch (e) {
-      if (e.response?.data) setError(e.response?.data);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function handleAdd(note) {
-    const res = await axios.post('/api/notes', note, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer SOMEJWTTOKEN',
-      },
-    });
-    setNotes([...notes, res.data]);
-  }
-
-  useEffect(() => {
-    const controller = new AbortController();
-    getNotes(null, controller);
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
   return (
     <main className='container'>
       <div>
-        <h1>我的笔记本</h1>
-        <SearchNote searchTerm={searchTerm} onChange={handleSearch} />
-        {loading ? <div>Loading...</div> : <NoteList notes={notes} />}
-        {error && <div style={{ color: 'red' }}>{error.message}</div>}
-        <AddNote onSubmit={handleAdd} />
+        <VideoPlayer />
       </div>
     </main>
   );
