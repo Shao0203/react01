ce9c6624b3855edba98aa8294c784a13f3cd6aab
Shao0203 <nzsyl0203@gmail.com>
2024-12-30 20:11:45 +0800

    useId() to generate unique id for HTML element id

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import { useRef, useState, useEffect } from 'react';
-import './App.css';
-import Sea from './assets/videos/sea.mp4';
-import { ReactComponent as Play } from './assets/icons/play.svg';
-import { ReactComponent as Pause } from './assets/icons/pause.svg';
-import VideoPlayer from './components/VideoPlayer';
+import { useId } from 'react';
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null); // useRef to get DOM
-
-  useEffect(() => {
-    videoRef.current.play();
-    setIsPlaying(true);
-  }, []);
-
-  function handlePlayClick() {
-    if (isPlaying) {
-      videoRef.current.pause();
-      setIsPlaying(false);
-    } else {
-      videoRef.current.play();
-      setIsPlaying(true);
-    }
-  }
-
+  const usernameId = useId();
   return (
     <main className='container'>
-      <div>
-        <VideoPlayer src={Sea} ref={videoRef} />
-        <button onClick={handlePlayClick}>
-          {isPlaying ? <Pause /> : <Play />}
-        </button>
-      </div>
+      <form>
+        <label htmlFor={usernameId + 'username'}>User name</label>
+        <input type='text' id={usernameId + 'username'} />
+      </form>
     </main>
   );
 }
