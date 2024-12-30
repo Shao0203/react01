ec92d5b4bf30d481194850b28dd881cc2630d5ab
Shao0203 <nzsyl0203@gmail.com>
2024-12-30 19:59:59 +0800

    use forwardRef to allow parent to get DOM from child component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
+import Sea from './assets/videos/sea.mp4';
+import { ReactComponent as Play } from './assets/icons/play.svg';
+import { ReactComponent as Pause } from './assets/icons/pause.svg';
 import VideoPlayer from './components/VideoPlayer';
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null); // useRef to get DOM
+
+  useEffect(() => {
+    videoRef.current.play();
+    setIsPlaying(true);
+  }, []);
+
+  function handlePlayClick() {
+    if (isPlaying) {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef.current.play();
+      setIsPlaying(true);
+    }
+  }
+
   return (
     <main className='container'>
       <div>
-        <VideoPlayer />
+        <VideoPlayer src={Sea} ref={videoRef} />
+        <button onClick={handlePlayClick}>
+          {isPlaying ? <Pause /> : <Play />}
+        </button>
       </div>
     </main>
   );
--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,36 +1,12 @@
-import React, { useRef, useState, useEffect } from 'react';
-import Sea from '../../assets/videos/sea.mp4';
-import { ReactComponent as Play } from './play.svg';
-import { ReactComponent as Pause } from './pause.svg';
+import { forwardRef } from 'react';
 import './style.css';
 
-function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null); // useRef to get DOM
-
-  useEffect(() => {
-    videoRef.current.play();
-    setIsPlaying(true);
-  }, []);
-
-  function handlePlayClick() {
-    if (isPlaying) {
-      videoRef.current.pause();
-      setIsPlaying(false);
-    } else {
-      videoRef.current.play();
-      setIsPlaying(true);
-    }
-  }
-
+function VideoPlayer({ src }, ref) {
   return (
     <div className='videoPlayer'>
-      <video src={Sea} width='320' ref={videoRef} muted />
-      <button onClick={handlePlayClick}>
-        {isPlaying ? <Pause /> : <Play />}
-      </button>
+      <video src={src} ref={ref} width='320' muted />
     </div>
   );
 }
 
-export default VideoPlayer;
+export default forwardRef(VideoPlayer);
