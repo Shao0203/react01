6cbb0f9250aef18bd92fdc32c1cb7d54f8fcee99
Shao0203 <nzsyl0203@gmail.com>
2025-01-03 21:48:15 +0800

    use web Animation API (native JS) to implement the same CSS keyframes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const refLink = useRef(null);
+
+  useEffect(() => {
+    if (refLink.current !== null) {
+      refLink.current.animate(
+        [{ opacity: 1 }, { opacity: 0.3 }, { opacity: 1 }],
+        { duration: 1000, iterations: Infinity }
+      );
+    }
+  }, []);
+
   return (
     <main className='container'>
-      <a href='#' className='link'>
+      <a href='#' className='link' ref={refLink}>
         超链接
       </a>
     </main>
