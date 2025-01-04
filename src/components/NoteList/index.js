e33589691f521d73280cd1db33c8ece1d2445515
Shao0203 <nzsyl0203@gmail.com>
2025-01-04 22:26:11 +0800

    highlight the selected active nav bar by using <NavLink> and its className isActive

--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './style.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NoteList({ notes }) {
   return (
     <nav className='noteList'>
       {notes.map((note) => (
         <div key={note.id} className='note'>
-          <Link to={`notes/${note.id}`}>{note.title}</Link>
+          <NavLink
+            to={`notes/${note.id}`}
+            className={({ isActive, isPending }) => (isActive ? 'active' : '')}
+          >
+            {note.title}
+          </NavLink>
           <p className='contentExerpt'>
             {note.content.substring(0, 20) + '...'}
           </p>
