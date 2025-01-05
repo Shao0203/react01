61126a94af019b3f43ac157adfb8d637acf1295b
Shao0203 <nzsyl0203@gmail.com>
2025-01-05 13:29:45 +0800

    useSubmit() hook for search bar to search on every input to avoid to click enter for searching every time

--- src/components/SearchNote/index.js
+++ src/components/SearchNote/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './style.css';
-import { Form } from 'react-router-dom';
+import { Form, useSubmit } from 'react-router-dom';
 
 function SearchNote() {
+  const submit = useSubmit();
+
   return (
     <div className='search'>
       <Form>
-        <input name='term' type='search' placeholder='搜索笔记' />
+        <input
+          name='term'
+          type='search'
+          placeholder='搜索笔记'
+          onChange={(e) => submit(e.target.form)}
+        />
       </Form>
     </div>
   );
